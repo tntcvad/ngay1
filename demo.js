6d8e4f39bddfd44e31d315e91a42427e516d85ce
tntcvad <48907412+tntcvad@users.noreply.github.com>
2019-03-26 15:51:46 +0700

    demo script

---
+++ demo.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML="xin chao ban den voi javascipt"
+}
+function hello() {
+    alert("xin chao")
+
+}
+function f() {
+    document.write("xin chao ban den voi ham write")
+}
+function b() {
+    console.log(alert("thong bao vi pham ban quyen"))
+}
