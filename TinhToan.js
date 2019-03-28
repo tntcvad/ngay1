6bebafec377aa64016608fd9e9d41aeb6aab4c12
tntcvad <48907412+tntcvad@users.noreply.github.com>
2019-03-28 16:25:58 +0700

    chuyen doi tien te

--- TinhToan.js
+++ TinhToan.js
@@ -1,29 +1,57 @@
 function tinh_tong() {
     //lấy dữ liệu từ trong bảng html;
-    let x = document.getElementById("x").value;
-    let y = document.getElementById("y").value;
+    let x = document.getElementById("txtNumber1").value;
+    let y = document.getElementById("txtNumber2").value;
     z = parseInt(x) + parseInt(y);
 
     document.getElementById("ketqua").value= z;
 }
 function tinh_hieu() {
-    let x = document.getElementById("x").value;
-    let y = document.getElementById("y").value;
+    let x = document.getElementById("txtNumber1").value;
+    let y = document.getElementById("txtNumber2").value;
     z = parseInt(x) - parseInt(y);
 
     document.getElementById("ketqua").value= z;
 }
 function multiplication() {
-    let x = document.getElementById("x").value;
-    let y = document.getElementById("y").value;
+    let x = document.getElementById("txtNumber1").value;
+    let y = document.getElementById("txtNumber2").value;
     z = parseInt(x) * parseInt(y);
 
     document.getElementById("ketqua").value= z;
 }
 function division() {
-    let x = document.getElementById("x").value;
-    let y = document.getElementById("y").value;
+    let x = document.getElementById("txtNumber1").value;
+    let y = document.getElementById("txtNumber2").value;
     z = parseInt(x) / parseInt(y);
 
     document.getElementById("ketqua").value= z;
 }
+function convertCurrency() {
+    let soTien = document.getElementById("amount").value ;
+    let fromccy = document.getElementById("fromCcy");
+    let fromCcyValue = fromccy.options[fromccy.selectedIndex].value;
+    let toccy = document.getElementById("toCcy") ;
+    let toccyValue = toccy.options[toccy.selectedIndex].value ;
+
+     //document.getElementById("result").value = soTien*23 ;
+    if (fromCcyValue == toccyValue) {
+        document.getElementById("result").value = soTien;
+    }
+    else
+        if(fromCcyValue == "vnd" ) {
+            document.getElementById("result").value = (soTien / 23);
+        }
+        else
+            document.getElementById("result").value = soTien * 23;
+
+
+
+
+
+
+
+
+
+}
+
